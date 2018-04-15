98f28876779ff6edf17454b6f1c664616d03628f
vlad.novikov <novikov.cs.vsu@gmail.com>
2018-04-15 16:12:31 +0300

    add code builders

---
+++ codegenerator/src/main/java/ru/vsu/codegenerator/builder/CodeBlockBuilder.java
@@ -0,0 +1,49 @@
+package ru.vsu.codegenerator.builder;
+
+import ru.vsu.codegenerator.builder.command.AssignCommandBuilder;
+import ru.vsu.codegenerator.builder.command.IfOperatorBuilder;
+import ru.vsu.codegenerator.builder.command.WhileOperatorBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeBlockBuilder implements CommandBuilder {
+
+    private List<CommandBuilder> lines = new ArrayList<>();
+
+    public void addAssignent(ExpressionBuilder lvalue, ExpressionBuilder rvalue){
+
+        lines.add(new AssignCommandBuilder(lvalue, rvalue));
+    }
+
+    public IfOperatorBuilder addIf(ExpressionBuilder condition){
+
+        IfOperatorBuilder builder = new IfOperatorBuilder(condition);
+
+        lines.add(builder);
+
+        return builder;
+    }
+
+    public WhileOperatorBuilder addWhile(ExpressionBuilder condition){
+
+        WhileOperatorBuilder builder = new WhileOperatorBuilder(condition);
+
+        lines.add(builder);
+
+        return builder;
+    }
+
+    @Override
+    public String getString(int tabulation) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for(CommandBuilder command : lines){
+
+            builder.append(command.getString(tabulation)).append("\n");
+        }
+
+        return builder.toString();
+    }
+}
---
+++ codegenerator/src/main/java/ru/vsu/codegenerator/builder/command/WhileOperatorBuilder.java
@@ -0,0 +1,36 @@
+package ru.vsu.codegenerator.builder.command;
+
+import ru.vsu.codegenerator.builder.CodeBlockBuilder;
+import ru.vsu.codegenerator.builder.CommandBuilder;
+import ru.vsu.codegenerator.builder.ExpressionBuilder;
+
+public class WhileOperatorBuilder implements CommandBuilder {
+
+
+    public WhileOperatorBuilder(ExpressionBuilder condition) {
+        this.condition = condition;
+    }
+
+    private ExpressionBuilder condition;
+    private CodeBlockBuilder codeBlockBuilder = new CodeBlockBuilder();
+
+    public CodeBlockBuilder getCodeBlockBuilder() {
+        return codeBlockBuilder;
+    }
+
+    @Override
+    public String getString(int tabulation) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i < tabulation; i++){
+
+            builder.append('\t');
+        }
+
+        builder.append("while ").append(condition).append(":\n")
+                .append(codeBlockBuilder.getString(tabulation + 1));
+
+        return builder.toString();
+    }
+}
