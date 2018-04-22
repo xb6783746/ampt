95b341879825873987a3a9dc946cf5cfde2bc06f
vlad.novikov <novikov.cs.vsu@gmail.com>
2018-04-22 19:37:39 +0300

    add cast results of standard functions to mlarr

--- analyser/src/main/java/ru/vsu/transformer/CompatibleFunctionTransformer.java
+++ analyser/src/main/java/ru/vsu/transformer/CompatibleFunctionTransformer.java
@@ -3,10 +3,13 @@ package ru.vsu.transformer;
 import ru.vsu.ast.BasicAstNode;
 import ru.vsu.ast.BasicAstVisitor;
 import ru.vsu.ast.expression.FunctionCallNode;
+import ru.vsu.ast.expression.FunctionCallNode.FunctionArgumentNode;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 public class CompatibleFunctionTransformer extends BasicAstVisitor<Void> implements AstTransformer {
@@ -44,8 +47,26 @@ public class CompatibleFunctionTransformer extends BasicAstVisitor<Void> impleme
         if(compatibleFunctions.containsKey(node.getFunctionName())){
 
             node.setFunctionName(compatibleFunctions.getProperty(node.getFunctionName()));
+
+            toMr(node);
         }
 
         return super.visit(node);
+    }
+
+    private void toMr(FunctionCallNode node){
+
+        BasicAstNode parent = node.getParent();
+
+        FunctionCallNode toMr = createMrNode(node);
+
+        parent.replace(node, toMr);
+    }
+
+    private FunctionCallNode createMrNode(FunctionCallNode node){
+
+        List<FunctionArgumentNode> arg = Collections.singletonList(new FunctionArgumentNode(node));
+
+        return new FunctionCallNode("mr", arg);
     }
 }
